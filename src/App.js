744069a3fd1251d66f08b31690d7462c44003c8c
Ryusei Namai <riry1313@gmail.com>
2020-04-05 15:09:26 +0900

    study JSX2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
+// JSXをトランスパイル時にReacを使うために必ず記述する。
 import React from 'react';
 
 function App() {
-  // ここがJSX
-  // 直感的に書ける。
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
-  // JSXについては下記のように書ける。
-  // return React.createElement(
-  //   "div",
-  //   null,
-  //   "Hello, World!!"
-  // )
+  // こんな風にも書ける
+  const dom = <h1>Hi!</h1>
+  return (dom);
 }
 
 export default App;
