96977147e4e43196423d8679baaa3aeb88d5b751
Ryusei Namai <riry1313@gmail.com>
2020-04-05 15:15:31 +0900

    study JSX2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import React from 'react';
 function App() {
   // こんな風にも書ける2
   const greeting = "Good Moirning!";
-  const dom = <h1>{greeting}</h1>
+  // Javascriptの予約後とかぶる
+  const dom = <h1 className="foo">{greeting}</h1>
   return (dom);
 }
 
