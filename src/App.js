f01fc44bfbee4a2a3e297cbeb12f3e5af5b40a27
Ryusei Namai <riry1313@gmail.com>
2020-04-05 15:31:07 +0900

    study JSX

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@
 import React from 'react';
 
 function App() {
+  // return で返すタグは一つではなければならない
+  // for も予約後なのでhtmlForで書く
   return (
     <div>
       <h1>Test dayo</h1>
+      <label htmlFor="bar">bar</label>
       <input type="text" onChange={() => {console.log("changed!!")} } />
     </div>
   );
