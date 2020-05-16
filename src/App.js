c632b2c467974041ca1fe3b4846a2fd294e49744
Ryusei Namai <riry1313@gmail.com>
2020-05-16 18:00:45 +0900

    change functional components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 // JSXをトランスパイル時にReacを使うために必ず記述する。
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Test dayo</h1>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("changed!!")} } />
-      </React.Fragment>
-    );
-  }
+// Class Component
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h1>Test dayo</h1>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("changed!!")} } />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// functional Component
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
