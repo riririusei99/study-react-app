6119aa9a678a77113e4ec4b41de7d992638bcbd6
Ryusei Namai <riry1313@gmail.com>
2020-05-16 18:27:20 +0900

    study props 4

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@
 import React from 'react';
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age:10},
+    { name: "Hanako", age:5}
+  ]
   return (
     <div>
-      <User name={"Taro"} age={10}/>
-      <br />
-      <User name={"Hanako"} age={5}/>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
 
 const User = (props) => {
-  return (
-    <React.Fragment>
-     Hi, I am {props.name}, and {props.age} years old!
-    </ React.Fragment>
-  )
+  return <div>Hi, I am {props.name}, and {props.age} years old!</ div>
 }
 
 export default App;
