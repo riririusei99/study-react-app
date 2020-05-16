cfe2135a80260da3d93b5a06e1cc63981fb44999
Ryusei Namai <riry1313@gmail.com>
2020-05-16 17:55:33 +0900

    change app.js to class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 // JSXをトランスパイル時にReacを使うために必ず記述する。
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  // return で返すタグは一つではなければならない
-  // 余分なdiv等で囲わないように<React.Fragment>を使う
-  // for も予約後なのでhtmlForで書く
-  return (
-    <React.Fragment>
-      <h1>Test dayo</h1>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("changed!!")} } />
-    </React.Fragment>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Test dayo</h1>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("changed!!")} } />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
