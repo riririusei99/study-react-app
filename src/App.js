008db9c2e3a34110f60a39b98b21ce7b1c01a71f
Ryusei Namai <riry1313@gmail.com>
2020-05-16 18:15:21 +0900

    study props

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 // JSXをトランスパイル時にReacを使うために必ず記述する。
 import React from 'react';
 
-// Class Component
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <h1>Test dayo</h1>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("changed!!")} } />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-// functional Component
 const App = () => {
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      <User name={"Taro"} />
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return (
+    <React.Fragment>
+     Hi, I am {props.name}!
+    </ React.Fragment>
+  )
 }
 
 export default App;
