f0c2fdb0b6ecb4a1bb8dcc4ed4bb477e8727ce3b
Ryusei Namai <riry1313@gmail.com>
2020-08-23 15:46:33 +0900

    add button element

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ class Counter extends Component {
     super(props)
     this.state = {count: 0}
   }
+  handlePlusButton = () => {
+    console.log("handlePlusButton")
+  }
+
   render() {
     return(
       <React.Fragment>
         <div>count: { this.state.count }</div>
-        <button>+1</button>
+        <button onClick={this.handlePlusButton}>+1</button>
         <button>-1</button>
       </ React.Fragment>
     )
