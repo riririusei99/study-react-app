e02b2315c58f31570f9e5d34feaa19d7b7fd1216
Ryusei Namai <riry1313@gmail.com>
2020-08-23 17:07:40 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Storeを作成するための関数をimportする
+import { createStore } from 'redux'
+// 作成したStoreを全コンポーネントに渡す機能を持つコンポーネントをimportする
+import { Provider} from 'react-dedux'
+// 作成したreducerをimportする
+import reducer from './reducers'
 import './index.css';
-import App from './App';
+// componentをimportする
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+// storeを作成する
+const store = createStore(reducer)
+
+// アプリケーションのcomponentでstoreが使えるようにする
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
