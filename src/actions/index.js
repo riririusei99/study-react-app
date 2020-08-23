de7b474a3ff755717f6f7c294456c48ea5e7452c
Ryusei Namai <riry1313@gmail.com>
2020-08-23 16:31:58 +0900

    create ActionCreator

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+// ActionCreator を定義する
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: 'INCREMENT'
+})
+
+export const decrement = () => ({
+  type: 'DECREMENT'
+})
